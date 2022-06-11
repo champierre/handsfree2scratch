903a7dacc4355cc91d0391aac781da4a62cd64fb
Junya Ishihara <junya@champierre.com>
2022-06-11 21:19:42 +0900

    Enable to get x and y of both hands.

--- src/vm/extensions/block/index.js
+++ src/vm/extensions/block/index.js
@@ -94,39 +94,97 @@ class ExtensionBlocks {
         scriptElem.src = 'https://unpkg.com/handsfree@8.5.1/build/lib/handsfree.js';
         document.head.appendChild(scriptElem);
 
+        this.hands = []
+
         setTimeout(() => {
-            const handsfree = new Handsfree({showDebug: true, hands: true});
+            const handsfree = new Handsfree({showDebug: false, hands: true});
             handsfree.start();
         
             // Create a plugin named "logger" to show data on every frame
             handsfree.use('logger', data => {
-                console.log(data.hands);
+                this.hands = data.hands;
+                console.log(this.hands.landmarks[0]);
             })
         }, 1000);
-
-
-
-        
     }
 
-    doIt (args) {
-        const func = new Function(`return (${Cast.toString(args.SCRIPT)})`);
-        const result = func.call(this);
-        console.log(result);
-        return result;
+    getLeftHandX (args) {
+        let landmark = args.LANDMARK - 1;
+        if (this.hands && this.hands.landmarks[0] && this.hands.landmarks[0][landmark]) {
+            if (this.runtime.ioDevices.video.mirror === false) {
+                return -1 * (240 - this.hands.landmarks[0][landmark].x * 480);
+            } else {
+                return 240 - this.hands.landmarks[0][landmark].x * 480;
+            }
+        } else {
+            return "";
+        }
     }
 
-    getX (args) {
-        console.log(args.LANDMARK);
+    getLeftHandY (args) {
+        let landmark = args.LANDMARK - 1;
+        if (this.hands && this.hands.landmarks[0] && this.hands.landmarks[0][landmark]) {
+            return 180 - this.hands.landmarks[0][landmark].y * 360;
+        } else {
+            return "";
+        }
     }
 
+    getRightHandX (args) {
+        let landmark = args.LANDMARK - 1;
+        if (this.hands && this.hands.landmarks[1] && this.hands.landmarks[1][landmark]) {
+            if (this.runtime.ioDevices.video.mirror === false) {
+                return -1 * (240 - this.hands.landmarks[1][landmark].x * 480);
+            } else {
+                return 240 - this.hands.landmarks[1][landmark].x * 480;
+            }
+        } else {
+            return "";
+        }
+    }
+
+    getRightHandY (args) {
+        let landmark = args.LANDMARK - 1;
+        if (this.hands && this.hands.landmarks[1] && this.hands.landmarks[1][landmark]) {
+            return 180 - this.hands.landmarks[1][landmark].y * 360;
+        } else {
+            return "";
+        }
+    }
+
+    videoToggle (args) {
+        let state = args.VIDEO_STATE;
+        if (state === 'off') {
+            this.runtime.ioDevices.video.disableVideo();
+        } else {
+            this.runtime.ioDevices.video.enableVideo();
+            this.runtime.ioDevices.video.mirror = state === "on";
+        }
+    }
 
     get LANDMARK_MENU () {
         let landmark_menu = [];
         for (let i = 1; i <= 1; i++) {
-            landmark_menu.push({text: `${formatMessage({id: 'handsfree2scratch.leftHand' + i})} (${i})`, value: String(i)})
+            landmark_menu.push({text: `${formatMessage({id: 'handsfree2scratch.handLandmark' + i})} (${i})`, value: i})
         }
         return landmark_menu;
+    }
+
+    get VIDEO_MENU () {
+        return [
+            {
+                text: formatMessage({id: 'handsfree2scratch.off'}),
+                value: 'off'
+            },
+            {
+                text: formatMessage({id: 'handsfree2scratch.on'}),
+                value: 'on'
+            },
+            {
+                text: formatMessage({id: 'handsfree2scratch.onFlipped'}),
+                value: 'on-flipped'
+            }
+        ]
     }
 
     /**
@@ -142,46 +200,104 @@ class ExtensionBlocks {
             showStatusButton: false,
             blocks: [
                 {
-                    opcode: 'do-it',
+                    opcode: 'getLeftHandX',
                     blockType: BlockType.REPORTER,
                     blockAllThreads: false,
                     text: formatMessage({
-                        id: 'handsfree2scratch.doIt',
-                        default: 'do it [SCRIPT]',
-                        description: 'execute javascript for example'
+                        id: 'handsfree2scratch.getLeftHandX',
+                        default: 'x of left hand [LANDMARK]',
+                        description: 'Get x of the selected left hand landmark'
                     }),
-                    func: 'doIt',
-                    arguments: {
-                        SCRIPT: {
-                            type: ArgumentType.STRING,
-                            defaultValue: '3 + 5'
-                        }
-                    }
-                },
-                {
-                    opcode: 'getX',
-                    blockType: BlockType.REPORTER,
-                    blockAllThreads: false,
-                    text: formatMessage({
-                        id: 'handsfree2scratch.getX',
-                        default: 'x of [LANDMARK]',
-                        description: 'x of the selected landmark'
-                    }),
-                    func: 'getX',
+                    func: 'getLeftHandX',
                     arguments: {
                         LANDMARK: {
                             type: ArgumentType.STRING,
                             menu: 'landmark',
-                            defaultValue: '0'
+                            defaultValue: 1
                         }
                     }
                 },
-
+                {
+                    opcode: 'getLeftHandY',
+                    blockType: BlockType.REPORTER,
+                    blockAllThreads: false,
+                    text: formatMessage({
+                        id: 'handsfree2scratch.getLeftHandY',
+                        default: 'y of left hand [LANDMARK]',
+                        description: 'Get y of the selected left hand landmark'
+                    }),
+                    func: 'getLeftHandY',
+                    arguments: {
+                        LANDMARK: {
+                            type: ArgumentType.STRING,
+                            menu: 'landmark',
+                            defaultValue: 1
+                        }
+                    }
+                },
+                {
+                    opcode: 'getRightHandX',
+                    blockType: BlockType.REPORTER,
+                    blockAllThreads: false,
+                    text: formatMessage({
+                        id: 'handsfree2scratch.getRightHandX',
+                        default: 'x of right hand [LANDMARK]',
+                        description: 'Get x of the selected right hand landmark'
+                    }),
+                    func: 'getRightHandX',
+                    arguments: {
+                        LANDMARK: {
+                            type: ArgumentType.STRING,
+                            menu: 'landmark',
+                            defaultValue: 1
+                        }
+                    }
+                },
+                {
+                    opcode: 'getRightHandY',
+                    blockType: BlockType.REPORTER,
+                    blockAllThreads: false,
+                    text: formatMessage({
+                        id: 'handsfree2scratch.getRightHandY',
+                        default: 'y of right hand [LANDMARK]',
+                        description: 'Get y of the selected right hand landmark'
+                    }),
+                    func: 'getRightHandY',
+                    arguments: {
+                        LANDMARK: {
+                            type: ArgumentType.STRING,
+                            menu: 'landmark',
+                            defaultValue: 1
+                        }
+                    }
+                },
+                {
+                    opcode: 'videoToggle',
+                    blockType: BlockType.COMMAND,
+                    blockAllThreads: false,
+                    text: formatMessage({
+                        id: 'handsfree2scratch.videoToggle',
+                        default: 'turn video [VIDEO_STATE]',
+                        description: 'Controls display of the video preview layer'
+                    }),
+                    func: 'videoToggle',
+                    arguments: {
+                        VIDEO_STATE: {
+                            type: ArgumentType.STRING,
+                            menu: 'video',
+                            defaultValue: 'off'
+                        }
+                    }
+                },
             ],
             menus: {
                 landmark: {
                     acceptReporters: true,
                     items: this.LANDMARK_MENU
+                },
+                video: {
+                    acceptReporters: true,
+                    items: this.VIDEO_MENU
                 }
             }
         };
